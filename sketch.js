fd8a5b7fecd5da157bc1e0715c9d9abc4c5f0c29
VARvalerie <52732660+VARvalerie@users.noreply.github.com>
2023-05-08 13:37:57 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,12 +65,12 @@ function setup() {
   
   myVida.addActiveZone(
     "zone0",
-    0, 0.5, 0.05, 0.3,
+    0.02, 0.4, 0.1, 0.3,
     trigger
   );
   myVida.addActiveZone(
     "zone1",
-    0, 0.5, 0.05, 0.1,
+    0.02, 0.5, 0.05, 0.1,
     trigger
   );
   myVida.addActiveZone(
